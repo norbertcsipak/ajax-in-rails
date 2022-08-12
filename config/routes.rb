471219ef8ced0332245f150a13f2105256a0e839
Norbert Csipak <norbertcipak@me.com>
2022-08-12 07:33:20 +0100

    Add review to restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  resources :restaurants, only: %i[index show]
+  resources :restaurants, only: %i[index show] do
+    resources :reviews, only: :create
+  end
 end
